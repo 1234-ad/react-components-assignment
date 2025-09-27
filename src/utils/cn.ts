1718d528fdfc6d5a677ae63fc763d6ac1aecdc39
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-27 12:13:00 +0530

    Add utility function for class names

---
+++ src/utils/cn.ts
@@ -0,0 +1,12 @@
+import { clsx, type ClassValue } from 'clsx';
+
+/**
+ * Utility function to merge class names with clsx
+ * @param inputs - Class names to merge
+ * @returns Merged class names
+ */
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs);
+}
+
+export default cn;
