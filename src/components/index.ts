c680e49ae6e2882cef20756c36675cf1d9ddf1de
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-27 12:09:46 +0530

    Add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// Export all components
+export { default as InputField } from './InputField/InputField';
+export type { InputFieldProps } from './InputField/InputField';
+
+export { default as DataTable } from './DataTable/DataTable';
+export type { DataTableProps, Column } from './DataTable/DataTable';
